d82fcafa7a4ed278af2bf893c62ee1c3c7926217
liangerdan <496703362@qq.com>
2018-12-19 19:34:03 +0800

    提交

--- Chapter11/src/main/java/com/course/controller/Demo.java
+++ Chapter11/src/main/java/com/course/controller/Demo.java
@@ -1,16 +1,15 @@
 package com.course.controller;
 
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.extern.log4j.Log4j;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+        import com.course.model.User;
+        import io.swagger.annotations.Api;
+        import io.swagger.annotations.ApiOperation;
+        import lombok.extern.log4j.Log4j;
+        import org.mybatis.spring.SqlSessionTemplate;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+        import javax.naming.Name;
 
 @Log4j
 @RestController
@@ -22,13 +21,26 @@ public class Demo {
     @Autowired
     private SqlSessionTemplate template;
 
-    @RequestMapping(value = "/getUserCount",method = RequestMethod.GET)
-    @ApiOperation(value = "可以获取到用户数",httpMethod = "GET")
-    public int getUserCount(){
+
+    @RequestMapping(value = "/getUserCount", method = RequestMethod.GET)
+    @ApiOperation(value = "可以获取到用户数", httpMethod = "GET")
+    public int getUserCount() {
         return template.selectOne("getUserCount");
     }
 
+    @RequestMapping(value = "/addUser", method = RequestMethod.POST)
+    public int addUser(@RequestBody User  user) {
+        return template.insert("addUser",user);
+    }
 
-
-
+    @RequestMapping(value = "/updateUser",method = RequestMethod.POST)
+    public int updateUser(@RequestBody User user){
+        return template.update("updateUser",user);
+    }
+    ;
+    @RequestMapping(value = "/deleterUser",method = RequestMethod.GET)
+    public int delUser(@RequestParam int age){
+      return template.delete("deleterUser",age); //("deleterUser",age);中的deleterUser是数据库中的对应的id,这个名字可以随便取，但是要和mysql.xml中的id一致
+    }
 }
+
